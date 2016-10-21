9dd1780faa2c0d2ce62ad3faecbcdd30ee2db8b0
votrunghau11 <vth1122@gmail.com>
2016-10-21 15:24:37 +0700

    Change background into Blue

--- BouncingBall.java
+++ BouncingBall.java
@@ -116,7 +116,7 @@ public class BouncingBall extends JFrame {
 		protected void paintComponent(Graphics g) {
 			long start = System.nanoTime();
 			super.paintComponent(g);
-			setBackground(Color.WHITE);
+			setBackground(Color.BLUE);
 			//Ball
 			g.setColor(ballColor);
 			g.fillOval(x, y, size, size);
